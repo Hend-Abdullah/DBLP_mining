14786bd49a8b3cd4cb0c1db3c43cd30240a028a7
Hend Abdullah <102986647+Hend-Abdullah@users.noreply.github.com>
2022-04-07 02:38:53 +0300

    Update dblp_code.py

--- dblp_code.py
+++ dblp_code.py
@@ -194,7 +194,7 @@ one_hot_encoder = pd.DataFrame(te_ary, columns=te.columns_)
 ## Page expands to full width
 st.set_page_config(layout="wide")
 #add logo
-image = Image.open('CSminer.jpg')
+image = Image.open('CS_miner.png')
 #st.image(image, width = 200)
 st.image(image, caption=None, width=300, use_column_width=None, clamp=False, channels="RGB", output_format="auto")
 st.title(""" Who are CS miner""")
