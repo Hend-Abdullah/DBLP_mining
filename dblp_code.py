3af0a781e1b6ef8abe17f6c4cab03046cf66556f
Hend Abdullah <102986647+Hend-Abdullah@users.noreply.github.com>
2022-04-06 23:37:53 +0300

    Update dblp_code.py

--- dblp_code.py
+++ dblp_code.py
@@ -193,7 +193,7 @@ coauthor_df['Common'] = co_authoring['common'].values
 ## Page expands to full width
 st.set_page_config(layout="wide")
 #add logo
-image = Image.open('CSminer.png')
+image = Image.open('CSM.jpg')
 st.image(image, width = 200, height=200)
 st.title(""" Who are CS miner""")
 st.subheader("Is a software that help all researcher or who are interset in computer science field to find career path and co-author relationships for a specific author or find top ten journals.")
